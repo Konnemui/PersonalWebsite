232bd29d21f17b9770a61179c06234743f75ee02
JedGardiner <Jgardinerp@gmail.com>
2026-04-06 22:01:14 +0100

    Monke work on website happi
    da monke work on da website

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # PersonalWebsite
-Web project for updates and development whenever I learn something new
-This is setup for me to iterate versions of this website possibly including entirely redesigning it.
 
-on hold due to studies in Japan, and final year
+## ToDo
+- Make fundamental design
+- basic content layout
+- ver 0.1
--- Script.js
+++ Script.js
@@ -1,8 +1,12 @@
 document.getElementById("chrome").addEventListener("mouseover", function() {
-    document.getElementById("chrome").style.backgroundColor = "black";
+    document.getElementById("chrome").style.backgroundColor = "";
 });
     
 document.getElementById("chrome").addEventListener("mouseout", function() {
-    document.getElementById("chrome").style.backgroundColor = "white";
+    document.getElementById("chrome").style.backgroundColor = "";
 });
 
+const mouse_cursor = document.addEventListener("mousemove");
+
+
+mouse_cursor.x
